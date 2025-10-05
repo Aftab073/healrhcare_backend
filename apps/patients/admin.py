311b3ccb13867150afd84b5373418417796d8caf
Aftab073 <tamboliaftab84@gmail.com>
2025-10-05 19:24:18 +0530

    register patients models in admin

--- apps/patients/admin.py
+++ apps/patients/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
+# apps/patients/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Patient
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    """Admin interface for Patient model"""
+    list_display = ['name', 'email', 'phone_number', 'blood_group', 'created_by', 'created_at']
+    list_filter = ['blood_group', 'created_at']
+    search_fields = ['name', 'email', 'phone_number']
+    readonly_fields = ['created_at', 'updated_at']
+    
+    fieldsets = (
+        ('Personal Information', {
+            'fields': ('name', 'email', 'phone_number', 'date_of_birth', 'address')
+        }),
+        ('Medical Information', {
+            'fields': ('blood_group', 'medical_history')
+        }),
+        ('System Information', {
+            'fields': ('created_by', 'created_at', 'updated_at')
+        }),
+    )
